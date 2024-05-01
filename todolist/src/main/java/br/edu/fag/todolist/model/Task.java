4e03c28d897e4ab2aa290cddba72febf362f7b4b
Ana Clara Bergamini Delgado <ana4ew.br@gmail.com>
2024-05-01 20:19:40 -0300

    Inserindo as informações necessárias na classe Task

--- todolist/src/main/java/br/edu/fag/todolist/model/Task.java
+++ todolist/src/main/java/br/edu/fag/todolist/model/Task.java
@@ -1,5 +1,45 @@
 package br.edu.fag.todolist.model;
 
+import java.util.Date;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+@Entity
+@Table(name = "tasks")
+@Data
+@AllArgsConstructor
+@ToString
 public class Task {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false)
+    private String title;
+
+    @Column(nullable=false)
+    private String description;
+
+    @Column(nullable = false)
+    private Date deadLine;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
 }
