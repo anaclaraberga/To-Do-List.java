7475d3cfe999ce1457cf15649227745234d0d54b
Ana Clara Bergamini Delgado <ana4ew.br@gmail.com>
2024-05-03 18:03:49 -0300

    Criação do CRUD

--- todolist/src/main/java/br/edu/fag/todolist/model/Task.java
+++ todolist/src/main/java/br/edu/fag/todolist/model/Task.java
@@ -13,12 +13,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Entity
 @Table(name = "tasks")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @ToString
 public class Task {
     
--- todolist/src/main/java/br/edu/fag/todolist/service/TaskService.java
+++ todolist/src/main/java/br/edu/fag/todolist/service/TaskService.java
@@ -1,6 +1,9 @@
 package br.edu.fag.todolist.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.edu.fag.todolist.model.Task;
@@ -16,5 +19,26 @@ public class TaskService {
 
     public Task createTask (Task task) {
         return taskRepository.save(task);
+    }
+
+    public List<Task> ListAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public ResponseEntity<Task> findTaskById(Long id) {
+        return taskRepository.findById(id)
+        .map(task -> ResponseEntity.ok().body(task))
+        .orElse(ResponseEntity.notFound().build());
+    }
+
+    public ResponseEntity<Task> updateTaskById(Task task, Long id) {
+        return taskRepository.findById(id)
+        .map(taskToUpdate ->{
+            taskToUpdate.setTitle(task.getTitle());
+            taskToUpdate.setDescription(task.getDescription());
+            taskToUpdate.setDeadLine(task.getDeadLine());
+            Task updated = taskRepository.save(taskToUpdate);
+            return ResponseEntity.ok().body(updated);
+        }).orElse(ResponseEntity.notFound().build());
     }
 }
