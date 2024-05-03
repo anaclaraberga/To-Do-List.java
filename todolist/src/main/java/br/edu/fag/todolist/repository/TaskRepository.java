8bbdc4cd11d0ad3a2865c6283a5416c397a8fb66
Ana Clara Bergamini Delgado <ana4ew.br@gmail.com>
2024-05-02 21:11:56 -0300

    Criação do service, repository e controller da classe Task

--- todolist/src/main/java/br/edu/fag/todolist/repository/TaskRepository.java
+++ todolist/src/main/java/br/edu/fag/todolist/repository/TaskRepository.java
@@ -1,5 +1,11 @@
 package br.edu.fag.todolist.repository;
 
-public interface TaskRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.edu.fag.todolist.model.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long>{
     
 }
--- todolist/src/main/java/br/edu/fag/todolist/service/TaskService.java
+++ todolist/src/main/java/br/edu/fag/todolist/service/TaskService.java
@@ -1,5 +1,20 @@
 package br.edu.fag.todolist.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.edu.fag.todolist.model.Task;
+import br.edu.fag.todolist.repository.TaskRepository;
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
 public class TaskService {
     
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public Task createTask (Task task) {
+        return taskRepository.save(task);
+    }
 }
