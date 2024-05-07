8c9d154258348664cceeaaec8560207330689db2
Ana Clara Bergamini Delgado <ana4ew.br@gmail.com>
2024-05-06 22:03:56 -0300

    Criando o "Delete By Id"

--- todolist/src/main/java/br/edu/fag/todolist/controller/TaskController.java
+++ todolist/src/main/java/br/edu/fag/todolist/controller/TaskController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,5 +47,11 @@ public class TaskController {
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Task> updateTaskById(@PathVariable(value = "id") Long id, @RequestBody Task task) {
         return taskService.updateTaskById(task, id);
+    }
+
+    @DeleteMapping("/tasks/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<Object> deleteTaskById(@PathVariable(value = "id") Long id) {
+        return taskService.deleteById(id);
     }
 }
