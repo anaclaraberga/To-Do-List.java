e3035b9f3546768b870fe936f215af6a08dd7985
Ana Clara Bergamini Delgado <ana4ew.br@gmail.com>
2024-05-07 17:23:59 -0300

    Implementação dos logs no Controller

--- todolist/src/main/java/br/edu/fag/todolist/controller/TaskController.java
+++ todolist/src/main/java/br/edu/fag/todolist/controller/TaskController.java
@@ -17,10 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 import br.edu.fag.todolist.model.Task;
 import br.edu.fag.todolist.service.TaskService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/api/v1")
 @AllArgsConstructor
+@Slf4j
 public class TaskController {
     
     TaskService taskService;
@@ -28,30 +30,35 @@ public class TaskController {
     @PostMapping("/tasks")
     @ResponseStatus(HttpStatus.CREATED)
     public Task createTask (@RequestBody Task task) {
+        log.info("Criando uma nova tarefa com as informações recebidas [{}]", task);
         return taskService.createTask(task);
     }
 
     @GetMapping("/tasks")
     @ResponseStatus(HttpStatus.OK)
     public List<Task> getAllTasks() {
+        log.info("Listando todas as tarefas cadastradas [{}]");
         return taskService.ListAllTasks();
     }
 
     @GetMapping("/tasks/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Task> getTaskById(@PathVariable(value = "id") Long id) {
+        log.info("Buscando tarefa com o id [{}]", id);
         return taskService.findTaskById(id);
     }
 
     @PutMapping("/tasks/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Task> updateTaskById(@PathVariable(value = "id") Long id, @RequestBody Task task) {
+        log.info("Atualizando tarefa com o id [{}]", id, task);
         return taskService.updateTaskById(task, id);
     }
 
     @DeleteMapping("/tasks/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public ResponseEntity<Object> deleteTaskById(@PathVariable(value = "id") Long id) {
+        log.info("Excluindo tarefa com o id [{}]", id);
         return taskService.deleteById(id);
     }
 }
